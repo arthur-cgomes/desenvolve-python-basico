a63a70c2807f67f5870068ccb40faa945c6931da
Arthur <contato.arthurdev@gmail.com>
2024-04-27 15:53:42 -0300

    2.2 - Tipos de dados

---
+++ modulo 2/2.2 - Tipos de dados/aula2_questao1.py
@@ -0,0 +1,13 @@
+# 1) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+# Imprima o tipo de dados dessas duas variáveis
+# Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+# Imprima a terceira variável e também o seu tipo
+
+var1 = 5
+var2 = 2
+print("Tipo de var1:", type(var1))
+print("Tipo de var2:", type(var2))
+
+var3 = var1 / var2
+print("O resultado da divisão é:", var3, "e o tipo é:", type(var3))
